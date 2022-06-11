f85dc78607b88b6f0457eec95abd32179f8389ff
Yuleni1 <myuleni4@gmail.com>
2022-06-10 23:16:27 -0400

    added weather elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@
 //query selectors
 
 var userFormEl= document.querySelector("#user-form");
-var userCityInputEl = document.querySelector("#username")
+var userCityInputEl = document.querySelector("#username");
+var cityEl = document.querySelector("#city-container");
+var weatherListEl = document.querySelector("#weather-list");
+
 
 
 //Go to documentation link, example of request, copy url, paste url in variable. onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
@@ -13,7 +16,7 @@ var getWeatherRepo = function(city){
 
     //format the github api url
     var apiUrl = 
-   "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&APPID=f1b34903d7c2cdaa6a859360c95e902f"
+   "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=imperial&APPID=f1b34903d7c2cdaa6a859360c95e902f"
 
  //if request was successful
 fetch(apiUrl)
@@ -21,8 +24,8 @@ fetch(apiUrl)
     if(response.ok){
         response.json().then(function(data){
             displayCities(data, city);
-            console.log("request successfull",data);
-            console.log("this should look like an array", response)
+            // console.log("request successfull",data);
+            // console.log("this should look like an array", response)
 //check if api has paginated issues
 
             if(response.headers.get("link")){
@@ -44,8 +47,8 @@ fetch(apiUrl)
 
 var formSubmitHandler = function(event){
     event.preventDefault();
-    console.log(event);
-
+    // console.log(event);
+    
     //input from user
     var cityInput = userCityInputEl.value.trim();
 
@@ -59,12 +62,49 @@ var formSubmitHandler = function(event){
 
 }
 
-var displayCities = function(city, searchTerm){
 
-    console.log("1",city);
-    console.log("2",searchTerm);
+var displayCities = function(city, searchTerm){
+   
+    weatherListEl.textContent="";
+    
+    // console.log("1",city);
+    // console.log("2",searchTerm);
+console.log("see if this works", city)
+ 
+// City Name  weatherListEl
+    var cityNameEl = document.createElement("h2");
+    var cityInfo = city.name;
+    cityNameEl.textContent =cityInfo;
+    cityEl.appendChild(cityNameEl);
+ 
+    
+  
+ 
+        
+        var ListEl = document.createElement("li");
+        var ListEl2 = document.createElement("li");
+        var ListEl3 = document.createElement("li");
+        var weatherTemp = city.main.temp;
+        var weatherWind = city.wind.speed;
+        var weatherHumid= city.main.humidity;
+        ListEl.textContent = ("Temp:" + weatherTemp)
+        ListEl2.textContent = ("Wind Speed" + weatherWind + "MPH")
+        ListEl3.textContent = ("Humidity" + weatherHumid);
+        weatherListEl.appendChild(ListEl);
+        weatherListEl.appendChild(ListEl2);
+        weatherListEl.appendChild(ListEl3);
+        
+        
+
+
+    
+  
+
 
 }
+
+
+    
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 
