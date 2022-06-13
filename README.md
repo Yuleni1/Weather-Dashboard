81f47c5ccc67ddaa57a63870d961f42e42f8a006
Yuleni1 <myuleni4@gmail.com>
2022-06-12 20:57:24 -0400

    added readme file

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Weather-Dashboard
+
+## Description
+This is a cahllange where the task has been given to create a weather dashboard by calling API's. The following acceptance criteria need to be met:
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+For the follwoing User Story: 
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+## Concept
+
+
+## Deployed Application
+https://yuleni1.github.io/Weather-Dashboard/
+
+
+![Weather Dashboard](/assets/Screenshot%202022-06-12%20205425.png)
+
+
+## Technologies Required
+
+* HTML
+* CSS
+* JAVASCRIPT
+
+## Tech Used
+
+* Github
+* Git Bash
+
+## Contribution
+
+Yuleni Martinez-Valenzuela
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,8 @@ var weatherListEl = document.querySelector("#weather-list");
 //Go to documentation link, example of request, copy url, paste url in variable. onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 
-
-var getWeatherRepo = function(city){
+//GETS WEATHER
+var getWeather = function(city){
 
     //format the github api url
     var apiUrl = 
@@ -24,9 +24,9 @@ fetch(apiUrl)
     if(response.ok){
         response.json().then(function(data){
             displayCities(data, city);
-            // console.log("request successfull",data);
-            // console.log("this should look like an array", response)
-//check if api has paginated issues
+            
+           
+    
 
             if(response.headers.get("link")){
                 console.log("bad link");
@@ -42,91 +42,12 @@ fetch(apiUrl)
     });
 };
 
-// getWeatherRepo("London")
 
-
-
-var geoCodeing = function(city){
-
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid=f1b34903d7c2cdaa6a859360c95e902f")
-    .then(function(response){
-        return response.json()
-
-    })
-
-    .then(function(data){
-        
-        // console.log("request succesful", data)
-        return data;
-    })
-}
-geoCodeing("london");
-
-
-var getWeatherForcast = function(city){
-    // fetch("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=imperial&appid=f1b34903d7c2cdaa6a859360c95e902f&cnt=1")
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&units=metric&appid=f1b34903d7c2cdaa6a859360c95e902f")
-    .then(function(response){
-        return response.json()
-
-    })
-
-    .then(function(data){
-    
-        console.log(data)
-
-      
-    for (var i = 0; i < data.list.length; i++){
-
-        var forcast = data.list[i]
-        
-
-        console.log("request succesful",forcast )
-
-
-
-
-}
-
-
-        return data;
-
-
-
-
-    })
-}
-
-
-getWeatherForcast("london")
-
-var formSubmitHandler = function(event){
-    event.preventDefault();
-    // console.log(event);
-    
-    //input from user
-    var cityInput = userCityInputEl.value.trim();
-
-    if (cityInput) {
-        getWeatherRepo(cityInput);
-        userCityInputEl.value = "";
-    }
-    else{
-        alert("Please enter a valid city");
-    }
-
-}
-
-
+//DISPLAYS WEATHER
 var displayCities = function(city, searchTerm){
    
     weatherListEl.textContent="";
     
-    // console.log("1",city);
-    // console.log("2",searchTerm);
-// console.log("see if this works", city)
- 
-// City Name  weatherListEl
     var cityNameEl = document.createElement("h2");
     var cityInfo = city.name;
     cityNameEl.textContent =cityInfo;
@@ -153,6 +74,48 @@ var displayCities = function(city, searchTerm){
 
 
 }
+
+//-------------------------------------------------------------------------------------------------------------------------
+//GETS GEO CODE
+var geoCodeing = function(city){
+
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid=f1b34903d7c2cdaa6a859360c95e902f")
+    .then(function(response){
+        return response.json()
+
+    })
+
+    .then(function(data){
+        
+        console.log("geocodeing", data)
+        return data;
+    })
+}
+geoCodeing("Texas")
+
+
+
+var getWeatherForcast = function(city){
+    
+     //fetch("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&exclude=current,hourly,minutely,alerts&units=imperial&appid=f1b34903d7c2cdaa6a859360c95e902f")
+    fetch("http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&exclude=current,hourly,minutely,alerts&units=imperial&appid=f1b34903d7c2cdaa6a859360c95e902f")
+    .then(function(response){
+        return response.json()
+
+    })
+
+    .then(function(data){
+    
+        console.log("get weather" , data)
+        return data
+    })
+}
+getWeatherForcast("Texas")
+
+
+
+
+
 
 var getForcast = function(city){
 
@@ -185,7 +148,24 @@ fetch(apiUrl)
 };
 
 
+//////////////////-----------------------------------------------------------------------------------------------
+//SUMITS CITY
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    // console.log(event);
+    
+    //input from user
+    var cityInput = userCityInputEl.value.trim();
 
+    if (cityInput) {
+        getWeather(cityInput);
+        userCityInputEl.value = "";
+    }
+    else{
+        alert("Please enter a valid city");
+    }
+
+}
 
     
 
